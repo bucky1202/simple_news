a2d2f320152c9c32102e33593109b555d3511109
bucky1202 <bakyegenov@gmail.com>
2024-07-25 13:03:16 +0500

    paginateed news 4

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -12,13 +12,13 @@ class IndexController extends Controller
     public function index(Request $request)
     {
         return view('user.index',[
-            'news' => News::latest()->paginate(3),
+            'news' => News::latest()->paginate(4),
         ]);
     }
 
     public function ajax_paginate(Request $request){
         return view('user.layouts.ajax-pagination',[
-            'news' => News::latest()->paginate(3),
+            'news' => News::latest()->paginate(4),
         ])->render();
     }
 }
